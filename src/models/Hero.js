a0de18761edb23a439554c06b19a76cd2182b1d3
Tim Falken <timfalken@hotmail.com>
2016-06-18 19:59:37 +0200

    NUKE OLD PROJECT

--- src/models/Hero.js
+++
@@ -1,8 +0,0 @@
-import {Model} from 'backbone';
-
-const Hero = Model.extend
-({
-	url: 'http://www.timfalken.com/heromanager/heroes/1'
-});
-
-export default Hero;
--- src/views.js
+++
@@ -1,4 +0,0 @@
-import _ from 'underscore;'
-import {Events} from 'backbone';
-import ClickA from './views/ClickA';
-import BoxBlock from './views/BoxBlock';
